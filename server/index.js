3c5183263f0f3090a1b4afb33543d33eeb31d118
Markus Risuaho <t1rima00@students.oamk.fi>
2022-11-16 20:01:40 +0200

    Visualisoinnit V1 #3 ja V2 #4 valmiit

--- server/index.js
+++ server/index.js
@@ -36,5 +36,69 @@ app.get("/globalmonthly",async function (req,res){
     }
     
 })
+app.get("/northernhemispherenannual",async function (req,res){
+    try{
+        const connection = await mysql.createConnection(config.db)
+        const [result] = await connection.execute('select * from northern_hemisphere_annual')
+
+        if (!result) result=[]
+        res.status(200).json(result)
+    } catch(err){
+        res.status(500).json({error:err.message})
+    }
+    
+})
+app.get("/northernhemispherenmonthly",async function (req,res){
+    try{
+        const connection = await mysql.createConnection(config.db)
+        const [result] = await connection.execute('select * from northern_hemisphere_monthly')
+
+        if (!result) result=[]
+        res.status(200).json(result)
+    } catch(err){
+        res.status(500).json({error:err.message})
+    }
+    
+})
+app.get("/southernhemispherenannual",async function (req,res){
+    try{
+        const connection = await mysql.createConnection(config.db)
+        const [result] = await connection.execute('select * from southern_hemisphere_annual')
+
+        if (!result) result=[]
+        res.status(200).json(result)
+    } catch(err){
+        res.status(500).json({error:err.message})
+    }
+    
+})
+app.get("/southernhemispherenmonthly",async function (req,res){
+    try{
+        const connection = await mysql.createConnection(config.db)
+        const [result] = await connection.execute('select * from southern_hemisphere_monthly')
+
+        if (!result) result=[]
+        res.status(200).json(result)
+    } catch(err){
+        res.status(500).json({error:err.message})
+    }
+    
+})
+app.get("/northernhemisphere2000",async function (req,res){
+    try{
+        const connection = await mysql.createConnection(config.db)
+        const [result] = await connection.execute('select * from norhem_2000_recon')
+
+        if (!result) result=[]
+        res.status(200).json(result)
+    } catch(err){
+        res.status(500).json({error:err.message})
+    }
+    
+})
+
+
+
+
 
 app.listen(port)
--- src/LampotilaTiedot.js
+++ src/LampotilaTiedot.js
@@ -7,6 +7,7 @@ import { Line } from "react-chartjs-2";
 
 const URL = 'http://localhost:3001/'
 export default function LampotilaTiedot() {
+        //GLOBAL ANNUAL
     const [tietoga, setTietoga] = useState ( [])
     useEffect(() => {
         axios.get(URL + 'globalannual')
@@ -16,29 +17,155 @@ export default function LampotilaTiedot() {
             alert(error.response.data.error)
         })
     }, [])
+    //GLOBAL MONTHLY
+    const [tietogm, setTietogm] = useState ( [])
+      useEffect(() => {
+          axios.get(URL + 'globalmonthly')
+          .then((response) => {
+              setTietogm(response.data)
+          }).catch (error => {
+              alert(error.response.data.error)
+          })
+      }, [])
+        //NORTHERN HEMISPHERE ANNUAL
+      const [tietonha, setTietonha] = useState ( [])
+      useEffect(() => {
+          axios.get(URL + 'northernhemispherenannual')
+          .then((response) => {
+              setTietonha(response.data)
+          }).catch (error => {
+              alert(error.response.data.error)
+          })
+      }, [])
+        //NORTHERN HEMISPHERE MONTHLY
+      const [tietonhm, setTietonhm] = useState ( [])
+      useEffect(() => {
+          axios.get(URL + 'northernhemispherenmonthly')
+          .then((response) => {
+              setTietonhm(response.data)
+          }).catch (error => {
+              alert(error.response.data.error)
+          })
+      }, [])
+      //SOUTHERN HEMISPHERE ANNUAL
+      const [tietosha, setTietosha] = useState ( [])
+      useEffect(() => {
+          axios.get(URL + 'southernhemispherenannual')
+          .then((response) => {
+              setTietosha(response.data)
+          }).catch (error => {
+              alert(error.response.data.error)
+          })
+      }, [])
+        //SOUTHERN HEMISPHERE MONTHLY
+      const [tietoshm, setTietoshm] = useState ( [])
+      useEffect(() => {
+          axios.get(URL + 'southernhemispherenmonthly')
+          .then((response) => {
+              setTietoshm(response.data)
+          }).catch (error => {
+              alert(error.response.data.error)
+          })
+      }, [])
+      //NORTHERN HEMISPHERE 2000 YEAR RECONSTRUCTION
+      const [tietonh2000, setTietonh2000] = useState ( [])
+      useEffect(() => {
+          axios.get(URL + 'northernhemisphere2000')
+          .then((response) => {
+              setTietonh2000(response.data)
+          }).catch (error => {
+              alert(error.response.data.error)
+          })
+      }, [])
 
 
 
 
-    const data = {
-        datasets : [
-            {
-            label: "Temperature Global Annual",
-            data: [tietoga],
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-        yAxisID: "Temp",
+  const data1 = {
+    datasets: [
+      {
+        label: "Global annual",
+        data: tietoga,
+        borderWidth: 2,
+        borderColor: "rgb(0, 0, 0)",
         parsing: {
-            xAxisKey: "Time",
-            yAxisKey: "Temp",
+          xAxisKey: "Time",
+          yAxisKey: "Temp",
         },
-            pointRadius: 1,
+        pointRadius: 1,
+      },
+      {
+        label: "Global monthly",
+        data: tietogm,
+        borderWidth: 2,
+        borderColor: "rgb(0, 0, 0)",
+        parsing: {
+          xAxisKey: "COL 1",
+          yAxisKey: "COL 2",
         },
-   
-    ],
-    };
+        pointRadius: 1,
+      },
+      {
+        label: "Northern Hemisphere Annual",
+        data: tietonha,
+        borderWidth: 2,
+        borderColor: "rgb(0, 0, 256)",
+        parsing: {
+          xAxisKey: "COL 1",
+          yAxisKey: "COL 2",
+        },
+        pointRadius: 1,
+      },
+      {
+        label: "Northern Hemisphere Monthly",
+        data: tietonhm,
+        borderWidth: 2,
+        borderColor: "rgb(0, 0, 256)",
+        parsing: {
+          xAxisKey: "COL 1",
+          yAxisKey: "COL 2",
+        },
+        pointRadius: 1,
+      },
+      {
+        label: "Southern Hemisphere Annual",
+        data: tietosha,
+        borderWidth: 2,
+        borderColor: "rgb(255, 0, 0)",
+        parsing: {
+          xAxisKey: "COL 1",
+          yAxisKey: "COL 2",
+        },
+        pointRadius: 1,
+      },
+      {
+        label: "Southern Hemisphere Monthly",
+        data: tietoshm,
+        borderWidth: 2,
+        borderColor: "rgb(255, 0, 0)",
+        parsing: {
+          xAxisKey: "COL 1",
+          yAxisKey: "COL 2",
+        },
+        pointRadius: 1,
+      },
+      {
+        label: "Northern Hemisphere 2000 year reconstruction",
+        data: tietonh2000,
+        hidden: true,
+        borderWidth: 2,
+        borderColor: "rgb(256, 0, 256)",
+        parsing: {
+          xAxisKey: "COL 1",
+          yAxisKey: "COL 2",
+        },
+        pointRadius: 1,
+      },
 
-const options = {
+    ],
+  };
+
+  const options = {
     responsive: true,
     plugins: {
       legend: {
@@ -46,12 +173,17 @@ const options = {
       },
       title: {
         display: true,
-        text: "Global Annual Graph Test",
+        text: "Temperature anomalies from January 1850 onwards",
       },
     },
     scales: {
-
-      Temp: {
+      x: {
+        type: "time",
+        time: {
+          unit: "month",
+        },
+      },
+      yAxis: {
         type: "linear",
       },
     },
@@ -59,10 +191,11 @@ const options = {
 
     return (
 
-             <div style={{ width: "1000px" }}>
+             <div style={{ width: "1200px" }}>
             <h3>Testidata</h3>
             
-            <Line options={options} data={data}/>
+            <Line options={options} data={data1}/>
+            
          
         
         
